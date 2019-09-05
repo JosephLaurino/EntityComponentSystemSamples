00c1c5255f3a4dbd7d9eb23532854a0d72c2d598
Joseph Laurino <joseph.laurino@gmail.com>
2019-09-04 19:14:07 -0700

    test to see how a system is added to ECS

---
+++ ECSSamples/Assets/HelloCube/1. ForEach/RotationSpeedSystem2_ForEach.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+// This system updates all entities in the scene with both a RotationSpeed_ForEach and Rotation component.
+
+// ReSharper disable once InconsistentNaming
+public class RotationSpeedSystem2_ForEach : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        // Entities.ForEach processes each set of ComponentData on the main thread. This is not the recommended
+        // method for best performance. However, we start with it here to demonstrate the clearer separation
+        // between ComponentSystem Update (logic) and ComponentData (data).
+        // There is no update logic on the individual ComponentData.
+        Entities.ForEach((ref RotationSpeed_ForEach rotationSpeed, ref Rotation rotation) =>
+        {
+            float3 axis;
+            axis.x = 1;
+            axis.y = 0;
+            axis.z = 0;
+            var deltaTime = Time.deltaTime;
+            rotation.Value = math.mul(math.normalize(rotation.Value),
+                quaternion.AxisAngle(axis, rotationSpeed.RadiansPerSecond * deltaTime));
+        });
+    }
+}
